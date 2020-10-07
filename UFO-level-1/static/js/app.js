b35638fe481dc4bea134c7ef595e6c56b2bd3653
jsabastian <john.santoyo@email.com>
2020-10-07 00:00:30 -0500

    commented parts of app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,16 +1,16 @@
 // from data.js
 var tableData = data;
-
+// used to select the first element that matches the specified selector string.
 let tbody = d3.select("tbody");
 
 function buildTable(data){
 
     tbody.html("");
-
+    // loop through the array
     data.forEach((dataRow) => {
-
+        // Create a Row dynamically inside the table from form data 
         let row = tbody.append("tr");
-               
+        // returns an array of values.  
         Object.values(dataRow).forEach((val) => {
            let cell = row.append("td");
            cell.text(val);
